9a0550cbc0b399506c8e13fa2e855137e21a8a8b
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2025-03-19 02:53:01 +0300

    Update content.tsx

--- src/website/content.tsx
+++ src/website/content.tsx
@@ -600,9 +600,9 @@ export const CONTENT = () => {
                      <li className={`flex items-center gap-4 text-lg ${lan === arabic && "lg:flex-row-reverse lg:mr-16"}`}>
                   
                   {lang === "arabic" ?
-                   <span className="text-right">فروعنا: البصرة وميسان والنجف الاشرف وكربلاء المقدسة وبابل</span>
+                   <span className="text-right"> فروعنا: واسط والبصرة والمثنى وميسان والنجف وكربلاء وبغداد وبابل </span>
                    :
-                   <span className="text-left"> Our branches: Basra, Maysan, Najaf Al-Ashraf, Karbala Al-Muqaddasa, and Babylon. </span>
+                   <span className="text-left"> Our branches: Wasit, Basra, Muthanna, Maysan, Najaf, Karbala, Baghdad, and Babylon. </span>
                   }
                  
                 </li>
